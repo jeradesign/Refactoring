3f70605748fab8edfba1f9ab389b5a8dd1d18aea
John Brewer <jbrewer@jera.com>
2022-03-25 16:25:50 -0700

    Code as of Chapter 17, end of page 219 (Immediately before we start adding tests.)

--- Refactoring/ViewController.swift
+++ Refactoring/ViewController.swift
@@ -11,9 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    // MARK: - Navigation
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        if segue.identifier == "changePassword" {
+            let changePasswordVC = segue.destination as? ChangePasswordViewController
+            changePasswordVC?.securityToken = "TOKEN"
+        }
+    }
 }
-
