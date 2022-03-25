2109c5e495318b59da2e860ef8228996e8aaf44b
John Brewer <jbrewer@jera.com>
2022-03-25 14:34:48 -0700

    Initial Commit

---
+++ Refactoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Refactoring
+//
+//  Created by John Brewer on 3/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RefactoringUITests/RefactoringUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RefactoringUITestsLaunchTests.swift
+//  RefactoringUITests
+//
+//  Created by John Brewer on 3/25/22.
+//
+
+import XCTest
+
+class RefactoringUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
